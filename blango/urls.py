770fbb89c14df8ce134cdd81b5acbd3ef10770ce
nn nn <nithya.semantic@gmail.com>
2022-05-02 02:42:52 +0000

    Finish First-Party Django REST API

--- blango/urls.py
+++ blango/urls.py
@@ -36,7 +36,8 @@ urlpatterns = [
     path("accounts/", include("allauth.urls")),
     path("ip/", blog.views.get_ip),
     path("", blog.views.index),
-    path("post/<slug>/", blog.views.post_detail, name="blog-post-detail")
+    path("post/<slug>/", blog.views.post_detail, name="blog-post-detail"),
+    path("api/v1/", include("blog.api_urls")),
 ]
 
 if settings.DEBUG:
